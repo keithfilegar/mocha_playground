041493aed9db9419db724be4b0b25cacf9455b4a
keithfilegar <keith.filegar@gmail.com>
2021-01-15 12:49:09 -0800

    setup test playground by installing mocha and chai to project

---
+++ test/divide.test.js
@@ -0,0 +1,13 @@
+const divide = require('../index');
+const chai = require('chai') //also works to remove line 3 and add '.expext' to end of this statement
+const expect = chai.expect;
+
+describe('Divide function', () => {
+    it('should divide positive integers correctly', () => {
+
+    });
+
+    it('should throw an error when divided by zero', () => {
+
+    });
+});
