8371c56954e0d949b38e19d3ebed1841a7f5d343
keithfilegar <keith.filegar@gmail.com>
2021-01-15 15:09:41 -0800

    add additional tests, one is intentionally failing for the sort function

--- test/divide.test.js
+++ test/divide.test.js
@@ -4,10 +4,25 @@ const expect = chai.expect;
 
 describe('Divide function', () => {
     it('should divide positive integers correctly', () => {
+        const a = 8;
+        const b = 4;
+        const expectedAnswer = 2;
 
+        const actualAnswer = divide(a, b);
+
+        expect(actualAnswer).to.equal(expectedAnswer);
+
+        //all of the above can be condensed to expect(divide)
     });
 
     it('should throw an error when divided by zero', () => {
+        const a =8, b =0;
+
+        const fn = () => {
+            divide(a, b)
+        }
+
+        expect(fn).to.throw();
 
     });
 });
---
+++ test/sort.test.js
@@ -0,0 +1,18 @@
+const sort = require('../index');
+const expect = require('chai').expect
+
+describe('Sort function', () => {
+    it('should sort an array of numbers in ascending order', () => {
+        const arr1 = [10, 31, 1, 22, 3]
+        const arr2 = [1, 3, 10, 22, 31]
+
+        expect(sort(arr1)).to.deep.equal(arr2)
+    })
+
+    it('should sort an array of numbers in ascending order if there are negative values', () => {
+        const arr1 = [-10, 31, 1, 22, 3]
+        const arr2 = [1, 3, -10, 22, 31]
+
+        expect(sort(arr1)).to.deep.equal(arr2)
+    })
+})
